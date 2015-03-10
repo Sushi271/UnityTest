ec1186ccc7cf4e06517caf24d1e58135f9576314
pawozakwa <pawozakwa@gmail.com>
2015-03-11 00:35:38 +0100

    tym razem na prawdę

---
+++ GitTestProject/Assets/Rotater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+    public float startY;
+	// Use this for initialization
+	void Start () {
+        startY = this.transform.localPosition.y;
+        StartCoroutine(rotateStart());
+	}
+
+    IEnumerator rotateStart () {
+        while(true){
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, startY + Mathf.Sin(Time.fixedTime) * 0.2F, this.transform.localPosition.z);
+            print("yeld");
+            this.transform.Rotate(new Vector3(Random.Range(-2, 5), Random.Range(-2, 5), Random.Range(-5,3)));
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
